674df0d3909b0efacd34dcda16b86a5c882a858f
Kevin Z. Snow <kevin@zeropointdynamics.com>
2020-06-02 15:50:56 -0400

    refactor for testability

--- src/zelos/ext/plugins/yarascan/yarascan.py
+++ src/zelos/ext/plugins/yarascan/yarascan.py
@@ -17,9 +17,15 @@
 
 import glob
 import os
+import pathlib
 
 from collections import defaultdict
+from io import StringIO
+from itertools import islice
 from pathlib import Path
+from typing import IO, Generator, List
+
+import zelos
 
 from zelos import CommandLineOption, IPlugin, Zelos
 
@@ -63,16 +69,166 @@ CommandLineOption(
 CommandLineOption(
     "yara_brief",
     action="store_true",
-    help="Only dump the list of matching rules, exclude match details.",
+    help="Only dump the list of matching rules, exclude match string details.",
 )
 CommandLineOption(
     "yara_xrefs",
     action="store_true",
     help="Count memory cross-references (pointers) for each match.",
 )
+CommandLineOption(
+    "yara_max",
+    type=int,
+    default=None,
+    help="Maximum number of yara matches per region (default: unlimited).",
+)
+
+
+class YaraString:
+    """ Represents a string component of a yara rule match. """
+
+    def __init__(self, address: int, yara_string, xrefs: int = 0):
+        self._s = yara_string
+        self._address = address
+        self._xrefs = xrefs
+
+    @property
+    def address(self) -> int:
+        return self._address
+
+    @property
+    def offset(self) -> int:
+        return self._s[0]
+
+    @property
+    def value(self) -> bytearray:
+        return self._s[2]
+
+    @property
+    def xrefs(self) -> int:
+        return self._xrefs
+
+
+class YaraMatch:
+    """ Represents a yara rule match. """
+
+    def __init__(
+        self,
+        yara_match,
+        process: zelos.processes.Process,
+        mr: zelos.MemoryRegion,
+        count_xrefs: bool = False,
+    ):
+        self._m = yara_match
+        self._pid = process.pid
+        self._process = process
+        self._mr = mr
+        self._strings = yara_match.strings
+        self._yara_strings = None
+        self._count_xrefs = count_xrefs
+        if count_xrefs:
+            self._xref_cnts = []
+            self._xref_total = 0
+            for s in self._strings:
+                cnt = self._find_xrefs(mr.start + s[0], process.memory)
+                self._xref_total += cnt
+                self._xref_cnts.append(cnt)
+
+    @property
+    def description(self) -> str:
+        desc = self._m.meta.get("description", None)
+        if desc is None:
+            desc = self._m.meta.get("Description", None)
+
+    @property
+    def namespace(self) -> str:
+        return self._m.namespace
+
+    @property
+    def rule(self) -> str:
+        return self._m.rule
+
+    @property
+    def pid(self) -> int:
+        return self._pid
+
+    @property
+    def region_desc(self) -> str:
+        return f"{self._mr}"
+
+    @property
+    def region_address(self) -> int:
+        return self._mr.start
+
+    @property
+    def xrefs(self) -> int:
+        return self._xref_total
+
+    @property
+    def strings(self) -> List[YaraString]:
+        if self._yara_strings is None:
+            self._yara_strings = [
+                YaraString(self.region_address + s[0], s, self._xref_cnts[i])
+                for i, s in enumerate(self._strings)
+            ]
+        return self._yara_strings
+
+    def _find_xrefs(self, address: int, memory: zelos.memory.Memory):
+        # TODO: count pointers to the given address across all memory
+        return 0
+
+    def yaml(self, brief: bool = False) -> str:
+        f = StringIO()
+        f.write(f"- {self.namespace}_{self.rule}:\n")
+        if self.description is not None:
+            f.write(f'\tdescription: "{self.description}"\n')
+        f.write(f"\tpid: {self.pid}\n")
+        f.write(f"\tnamespace: {self.namespace}\n")
+        f.write(f"\trule: {self.rule}\n")
+        f.write(f'\tregion_desc: "{self.region_desc}"\n')
+        f.write(f"\tregion_address: 0x{self.region_address:x}\n")
+        if self._count_xrefs:
+            f.write(f"\txrefs: {self.xrefs}\n")
+        if not brief:
+            f.write(f"\tstrings:\n")
+            for i, s in enumerate(self.strings):
+                f.write(f"\t\taddress: 0x{self._mr.start + s.offset:x}\n")
+                f.write(f"\t\t\toffset: 0x{s.offset:x}\n")
+                f.write(f'\t\t\tvalue: "{s.value[:1000]}"\n')
+                if self._count_xrefs:
+                    f.write(f"\t\t\txrefs: {self._xref_cnts[i]}\n")
+        return f.getvalue()
+
+    def info(self, brief: bool = False) -> str:
+        if brief:
+            return f"Matched rule: {self.namespace}.{self.rule}"
+        else:
+            for i, s in enumerate(self.strings):
+                xref_info = ""
+                if self._count_xrefs:
+                    xref_info = f" xrefs: {self._xref_cnts[i]}"
+                return (
+                    f"Matched {self.namespace}.{self.rule}"
+                    f"0x{s.address:08x} +0x{s.offset:x}{xref_info} "
+                    f"{s.value[:100]}"
+                )
+
+    def memdump(self, directory: str) -> str:
+        filename = os.path.join(
+            directory, f"PID{self.pid}_0x{self.region_address:x}.mem"
+        )
+        pathlib.Path(filename).parent.mkdir(parents=True, exist_ok=True)
+        with open(filename, "wb") as f:
+            f.write(self._mr.get_data())
+            return filename
+        return None
 
 
 class YaraScan(IPlugin):
+    """
+    YaraScan plugin scans memory using Yara rules.
+    """
+
     def __init__(self, z: Zelos):
         super().__init__(z)
         self._rules = None
@@ -95,22 +251,160 @@ class YaraScan(IPlugin):
             return
         self._z = z
 
-        def closure():
-            self._load_rules(
-                z.config.yara_file, z.config.yara_rule, z.config.yara_file_glob
+        def closure() -> None:
+            self.compile(
+                files=z.config.yara_file,
+                rules=z.config.yara_rule,
+                glob_string=z.config.yara_file_glob,
             )
             self._log("scanning memory...")
-            self._scan(pid=z.config.yara_pid, brief=z.config.yara_brief)
-            self._log(f"Wrote matches to: {z.config.yara_outfile}")
+            all(
+                islice(
+                    self.matches(
+                        pid=z.config.yara_pid,
+                        yamldump=z.config.yara_outfile,
+                        memdump=z.config.yara_memdump,
+                        brief=z.config.yara_brief,
+                        xrefs=z.config.yara_xrefs,
+                    ),
+                    z.config.yara_max,
+                )
+            )
 
         z.hook_close(closure)
 
-    def _log(self, s):
+    def _log(self, s: str) -> None:
         self._z.logger.info(f"{s}")
 
-    def _load_rules(
+    def _get_regions(
+        self, memory: zelos.memory.Memory
+    ) -> Generator[zelos.MemoryRegion, None, None]:
+        for mr in sorted(memory.get_regions()):
+            if mr.kind == "heap":
+                continue
+            yield mr
+
+    def _match(self, data: bytes):
+        if self._cmdline_rules is not None:
+            for m in self._cmdline_rules.match(data=data):
+                yield m
+        if self._rules is not None:
+            for m in self._rules.match(data=data):
+                yield m
+
+    def _scan_region(
+        self,
+        mr: zelos.MemoryRegion,
+        process: zelos.processes.Process,
+        memdump: str = None,
+        yamldump: str = None,
+        brief=False,
+        xrefs=False,
+    ) -> Generator[YaraMatch, None, None]:
+        for m in self._match(bytes(mr.get_data())):
+            match = YaraMatch(m, process, mr, xrefs)
+            self._log(match.info(brief))
+            if memdump is not None:
+                self._log(f"Wrote {match.memdump(memdump)}")
+            if yamldump is not None:
+                yamldump.write(match.yaml(brief))
+            yield match
+
+    def _scan_process(
+        self,
+        process: zelos.processes.Process,
+        memdump: str = None,
+        yamldump: IO = None,
+        brief: bool = False,
+        xrefs: bool = False,
+    ) -> Generator[YaraMatch, None, None]:
+        for mr in self._get_regions(process.memory):
+            if not brief:
+                self._log(f"PID:{process.pid} {mr}")
+            yield from self._scan_region(
+                mr,
+                process,
+                memdump=memdump,
+                yamldump=yamldump,
+                brief=brief,
+                xrefs=xrefs,
+            )
+
+    def _scan_processes(
+        self,
+        pid: int = None,
+        memdump: str = None,
+        yamldump: IO = None,
+        brief: bool = False,
+        xrefs: bool = False,
+    ) -> Generator[YaraMatch, None, None]:
+        for process in self._z.internal_engine.processes.process_list:
+            if pid is not None and process.pid != pid:
+                continue
+            yield from self._scan_process(
+                process,
+                memdump=memdump,
+                yamldump=yamldump,
+                brief=brief,
+                xrefs=xrefs,
+            )
+
+    def matches(
+        self,
+        pid: int = None,
+        memdump: str = None,
+        yamldump: str = None,
+        brief: bool = False,
+        xrefs: bool = False,
+    ) -> Generator[YaraMatch, None, None]:
+        """
+        Scan memory for the previously compiled yara rules.
+
+        Args:
+            pid: process ID to scan. All processes scanned if `None`.
+            memdump: directory to dump matching regions, or `None`.
+            yamldump: yaml output file, or `None`.
+            brief: if True, only output rule matches.
+            xrefs: if True, count number of references to each match.
+
+        Returns:
+            A generator yielding YaraMatch.
+        """
+        if yamldump is not None:
+            pathlib.Path(yamldump).parent.mkdir(parents=True, exist_ok=True)
+            with open(yamldump, "w") as f:
+                yield from self._scan_processes(
+                    pid=pid,
+                    memdump=memdump,
+                    yamldump=f,
+                    brief=brief,
+                    xrefs=xrefs,
+                )
+                self._log(f"Wrote matches in yaml format to: {yamldump}")
+        else:
+            yield from self._scan_processes(
+                pid=pid,
+                memdump=memdump,
+                yamldump=None,
+                brief=brief,
+                xrefs=xrefs,
+            )
+
+    def compile(
         self, files: str = [], rules: str = [], glob_string: str = None
-    ):
+    ) -> int:
+        """
+        Compile yara rules.
+
+        Args:
+            files: list of yara rule files to compile.
+            rules: list of strings that will create rules on-the-fly.
+            glob_string: python glob file string. All matching files
+                will be included as rule files.
+
+        Returns:
+            The number of files and rules loaded.
+        """
         if files is None:
             files = []
         if rules is None:
@@ -141,34 +435,4 @@ class YaraScan(IPlugin):
         )
         self._rules = self._yara.compile(filepaths=filepaths)
         self._cmdline_rules = self._yara.compile(sources=sources)
-
-    def _count_xrefs(self, address):
-        # TODO: count pointers to the given address across all memory
-        return 0
-
-    def _match(self, data):
-        for m in self._cmdline_rules.match(data=data):
-            yield m
-        for m in self._rules.match(data=data):
-            yield m
-
-    def _scan(self, pid=None, brief=False):
-        for process in self._z.internal_engine.processes.process_list:
-            if pid is not None and process.pid != pid:
-                continue
-            memory = process.memory
-            for mr in sorted(memory.get_regions()):
-                if mr.kind == "heap":
-                    continue
-                if not brief:
-                    self._log(f"PID:{process.pid} {mr}")
-                for m in self._match(bytes(mr.get_data())):
-                    for s in m.strings:
-                        offset = s[0]
-                        value = s[2]
-                        address = mr.start + offset
-                        if not brief:
-                            self._log(
-                                f"Matched {m.namespace}.{m.rule} "
-                                f"0x{address:08x} +0x{offset:x} {value[:100]}"
-                            )
+        return len(files) + len(rules)
